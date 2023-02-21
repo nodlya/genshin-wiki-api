da7973377f06014d6b77b6b47a8a279d30ccf891
Slava <kruchkovenko@gmail.com>
2023-02-21 20:53:06 +0500

    add correct connect to mongo db

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || config.port;
-const db = `mongodb+srv://${config.dbUser.userName}:${config.dbUser.password}@cluster0.czj4pwj.mongodb.net/?retryWrites=true&w=majority`
+const db = `mongodb+srv://${config.dbUser.userName}:${config.dbUser.password}@cluster0.czj4pwj.mongodb.net/Genshin_wiki_database?retryWrites=true&w=majority`;
 
 const initDb = () => {
+    mongoose.set('strictQuery', true);
     mongoose
         .connect(db)
         .then(()=>{
             console.log("database ok")
         })
         .catch(e=>{
-            console.log("database err",e)
+            console.log("database err\n",e)
         })
 }
-const main = () => {
-    initDb()
+const initClient = () => {
     app.use(express.json());
     app.use(
         cors({
@@ -32,10 +32,20 @@ const main = () => {
             ],
         })
     );
-    app.get('/',()=>{
+
+    app.listen(port,()=>{console.log(`server start on http://localhost:${port}`)})
+}
+const initRouters = () => {
+    app.get('/',(req,res)=>{
         console.log("hello world")
     })
-    app.listen(port,()=>{console.log("server start")})
+    //ToDo add routers to app
+}
+
+const main = () => {
+    initDb()
+    initRouters()
+    initClient()
 }
 
 main()
