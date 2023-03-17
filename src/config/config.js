33ddbaaf6e4356e7031f68c1256eef4e32c8363e
Slava <sh-e-95@mail.ru>
2023-03-17 18:17:23 +0500

    delete ignore

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+const config = {
+    port:4002,
+    dbUser:{
+        userName: "Slava228",
+        password: "9MA3Vz6JKeX6wkTe"
+    },
+    databaseName: "Genshin_wiki_database"
+}
+module.exports = config
